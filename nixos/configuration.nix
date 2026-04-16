866b4bdffd9b6637b77fea04324475813021abde
louis.rodet <louis.rodet@epita.fr>
2026-04-16 22:50:59 +0200

    move config and hardware config to /nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -288,21 +288,21 @@
     };
   
     # nixpkgs.config.allowUnfree = true; 
-    xdg.configFile."hypr/hyprland.conf".source = ./dotfiles/hyprland.conf; # Hyprland
-    xdg.configFile."waybar/config".source = ./dotfiles/waybar-config; # Waybar (Fichier config)
-    xdg.configFile."waybar/style.css".source = ./dotfiles/waybar-style.css; # Waybar (Fichier css)
+    xdg.configFile."hypr/hyprland.conf".source = ../dotfiles/hyprland.conf; # Hyprland
+    xdg.configFile."waybar/config".source = ../dotfiles/waybar-config; # Waybar (Fichier config)
+    xdg.configFile."waybar/style.css".source = ../dotfiles/waybar-style.css; # Waybar (Fichier css)
     xdg.configFile."waybar/switch-audio-sink.sh" = {
-      source = ./dotfiles/switch-audio-sink.sh;
+      source = ../dotfiles/switch-audio-sink.sh;
       executable = true;
     };
-    xdg.configFile."rofi/config.rasi".source = ./dotfiles/rofi/config.rasi; # Rofi config
-    xdg.configFile."rofi/themes/calm.rasi".source = ./dotfiles/rofi/calm.rasi; # Rofi theme
-    xdg.configFile."hypr/hyprlock.conf".source = ./dotfiles/hyprlock.conf; # Hyprlock (Screen locker)
-    xdg.configFile."hypr/hyprpaper.conf".source = ./dotfiles/hyprpaper.conf; # Hyprpaper (Wallpaper)
-    xdg.configFile."hypr/wallpapers".source = ./dotfiles/wallpapers;
-    home.file.".zsh-powerline.sh".source = ./dotfiles/.zsh-powerline.sh;
-    home.file.".zshrc".source = ./dotfiles/.zshrc;
-    home.file.".vimrc".source = ./dotfiles/.vimrc;
+    xdg.configFile."rofi/config.rasi".source = ../dotfiles/rofi/config.rasi; # Rofi config
+    xdg.configFile."rofi/themes/calm.rasi".source = ../dotfiles/rofi/calm.rasi; # Rofi theme
+    xdg.configFile."hypr/hyprlock.conf".source = ../dotfiles/hyprlock.conf; # Hyprlock (Screen locker)
+    xdg.configFile."hypr/hyprpaper.conf".source = ../dotfiles/hyprpaper.conf; # Hyprpaper (Wallpaper)
+    xdg.configFile."hypr/wallpapers".source = ../dotfiles/wallpapers;
+    home.file.".zsh-powerline.sh".source = ../dotfiles/.zsh-powerline.sh;
+    home.file.".zshrc".source = ../dotfiles/.zshrc;
+    home.file.".vimrc".source = ../dotfiles/.vimrc;
     
     # (Optionnel) Pour s'assurer que Waybar est bien géré
     programs.waybar.enable = true;
