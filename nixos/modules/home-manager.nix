767dd6878e1faf1c257de1dfeab5d8360084a364
louis.rodet <louis.rodet@epita.fr>
2026-04-18 21:34:11 +0200

    refactor: dotfiles architecture for home manager

--- nixos/modules/home-manager.nix
+++ nixos/modules/home-manager.nix
@@ -1,5 +1,8 @@
 { ... }:
 
+let
+  dotfiles = ../../dotfiles;
+in
 {
   home-manager.users.louis = { ... }:
   {
@@ -11,21 +14,29 @@
       email = "louis.rodet@epita.fr";
     };
 
-    xdg.configFile."hypr/hyprland.conf".source = ../../dotfiles/hyprland.conf;
-    xdg.configFile."waybar/config".source = ../../dotfiles/waybar-config;
-    xdg.configFile."waybar/style.css".source = ../../dotfiles/waybar-style.css;
+    # Editor
+    home.file.".vimrc".source = dotfiles + "/editor/.vimrc";
+
+    # hypr
+    xdg.configFile."hypr/hyprland.conf".source = dotfiles + "/hypr/hyprland.conf";
+    xdg.configFile."hypr/hyprlock.conf".source = dotfiles + "/hypr/hyprlock.conf";
+    xdg.configFile."hypr/hyprpaper.conf".source = dotfiles + "/hypr/hyprpaper.conf";
+    xdg.configFile."hypr/wallpapers".source = dotfiles + "/hypr/wallpapers";
+
+    # Rofi
+    xdg.configFile."rofi/config.rasi".source = dotfiles + "/rofi/config.rasi";
+    xdg.configFile."rofi/themes/calm.rasi".source = dotfiles + "/rofi/calm.rasi";
+
+    # Shell
+    home.file.".zsh-powerline.sh".source = dotfiles + "/shell/.zsh-powerline.sh";
+    home.file.".zshrc".source = dotfiles + "/shell/.zshrc";
+
+    # Waybar
+    xdg.configFile."waybar/config".source = dotfiles + "/waybar/config";
+    xdg.configFile."waybar/style.css".source = dotfiles + "/waybar/style.css";
     xdg.configFile."waybar/switch-audio-sink.sh" = {
-      source = ../../dotfiles/switch-audio-sink.sh;
+      source = dotfiles + "/waybar/switch-audio-sink.sh";
       executable = true;
     };
-    xdg.configFile."rofi/config.rasi".source = ../../dotfiles/rofi/config.rasi;
-    xdg.configFile."rofi/themes/calm.rasi".source = ../../dotfiles/rofi/calm.rasi;
-    xdg.configFile."hypr/hyprlock.conf".source = ../../dotfiles/hyprlock.conf;
-    xdg.configFile."hypr/hyprpaper.conf".source = ../../dotfiles/hyprpaper.conf;
-    xdg.configFile."hypr/wallpapers".source = ../../dotfiles/wallpapers;
-
-    home.file.".zsh-powerline.sh".source = ../../dotfiles/.zsh-powerline.sh;
-    home.file.".zshrc".source = ../../dotfiles/.zshrc;
-    home.file.".vimrc".source = ../../dotfiles/.vimrc;
   };
 }
