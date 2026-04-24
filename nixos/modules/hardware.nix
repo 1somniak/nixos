e10dc7d558edf02d9eee7b06828f3febde19f89b
louis.rodet <louis.rodet@epita.fr>
2026-04-24 20:45:16 +0200

    feat: update boot to have less messages

--- nixos/modules/hardware.nix
+++ nixos/modules/hardware.nix
@@ -2,8 +2,23 @@
 
 {
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+
+    # Enable silent mode
+    consoleLogLevel = 0;
+    kernelParams = [
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+  };
 
   # Swapfile 24 Go
   swapDevices = [
